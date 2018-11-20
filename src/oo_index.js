03e34e7ef5d4f0da920727ddaa7b47c6990437e0
github username <chao.alexander.123@gmail.com>
2018-11-20 18:05:26 -0500

    create functions to intercept form submission and extract the value from the input to update and delete the todo list using OO approach

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,23 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+
+  const taskUl = document.getElementById("tasks");
+
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+  newTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    taskList.createTask(newTaskDescription.value);
+    event.target.reset();
+    renderApp();
+  });
+
+  taskUl.addEventListener("click", (event) => {
+    if (event.target.nodeName === "BUTTON") {
+      taskList.deleteTask(event.target.dataset.description);
+      renderApp();
+    }
+  });
 });
